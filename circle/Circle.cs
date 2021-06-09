2f3cebec6c04d3c38d598be7aa1bf257ec60a9e8
Soumay kanti Das <anikds2859@outlook.com>
2021-06-09 17:45:48 +0600

    Add project files.

---
+++ circle/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    public class Circle
+    {
+        public double Radious { get; set; }
+        public double FindArea()
+        {
+              return Math.PI * Radious * Radious;
+        }
+        public double Findcircumestance()
+        {
+            return Math.PI * 2 * Radious;
+        }
+
+    }
+}
---
+++ circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b;
+
+            double a;
+            b = Console.ReadLine();
+            a = Convert.ToDouble(b);
+
+            Circle circle = new Circle();
+            circle.Radious = a ;
+            double area = circle .FindArea();
+            double cicumestence = circle.Findcircumestance();
+            Console.WriteLine(area);
+            Console.WriteLine(cicumestence);
+        }
+    }
+}
